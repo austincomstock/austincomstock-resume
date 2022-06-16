87ec3e6cfca0682c4de9bd5e5c1cd9828469afb9
austincomstock <austinjcomstock@gmail.com>
2022-06-16 16:49:02 -0600

    Shortened length of job description

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -10,18 +10,11 @@ export const Experience = () => {
             <h3 className="mb-0">Operations Manager</h3>
             <div className="subheading mb-3">Enova International</div>
             <p>
-              Successfully onboarded team of fourteen new representatives during
-              the initial wave of COVID-19, our team was in-office for one day
-              before moving to a work from home environment. This was a success
-              due to setting expectations early with the team and focusing on
-              each representatives’ personal goals during weekly meetings. 21%
-              of the representatives from my initial new hire team have since
-              been promoted into internal management roles. Identified need and
-              spearheaded creation of internal Wiki/Confluence pages to optimize
-              the onboarding and training process for NetCredit leadership. This
-              dramatically improved the onboarding process for section managers
-              and the leadership team due to all of the content being available
-              in one location.
+              Identified need and spearheaded creation of internal
+              Wiki/Confluence pages to optimize the onboarding and training
+              process for leadership. This dramatically improved the onboarding
+              process for managers and the leadership team due to all of the
+              content being available in one location.
             </p>
           </div>
           <div className="flex-shrink-0">
@@ -33,13 +26,12 @@ export const Experience = () => {
             <h3 className="mb-0">Quality Assurance Manager</h3>
             <div className="subheading mb-3">Upwell Health</div>
             <p>
-              Brought in to restructure QA department which was cut from twenty
-              agents down to five. Successfully improved call auditing
-              percentage with 75% less staff, increasing from 6% of total calls
-              audited to 30% by leading the implementation of a call auditing
-              software to support the limited staff. Played a major role in
-              creating processes within the Compliance and Human Resource
-              departments to identify disciplinary guidelines for
+              Brought in to restructure QA department. Successfully improved
+              call auditing percentage with 75% less staff, increasing from 6%
+              of total calls audited to 30% by leading the implementation of a
+              call auditing software to support the limited staff. Played a
+              major role in creating processes within the Compliance and Human
+              Resource departments to identify disciplinary guidelines for
               representatives failing to meet compliance standards along with
               recognition processes to highlight outstanding performance.
             </p>
@@ -57,7 +49,7 @@ export const Experience = () => {
               complaints from customers threating or taking legal action against
               the company. Developed reporting to track the complaint pipeline
               along with a secondary process to escalate high level complaints
-              to the executive group.Drafted response letters addressed to BBB,
+              to the executive group. Drafted response letters addressed to BBB,
               AG, BOP and other agencies for our in-house counsel to resolve
               complaints resulting in higher business ratings and customer
               satisfaction.

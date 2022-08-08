2ce7dc5d12b03034418ed305983c6e5ce816f4ff
austincomstock <austinjcomstock@gmail.com>
2022-08-08 11:49:13 -0600

    Updated spelling

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -16,7 +16,7 @@ export const About = () => {
           </a>
         </div>
         <p className="lead mb-5">
-          Hello my name is Austin, I'm a Front End Web Developer passionate
+          Hello my name is Austin, I'm a Front-End Web Developer passionate
           about building and being creative. Interested in working together?
         </p>
         <p className="lead mb-5">Github/Social Links:</p>

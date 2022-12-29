c69735229f202ea0cf91ae9539acc63e8ccfb32c
austincomstock <austinjcomstock@gmail.com>
2022-12-29 10:27:58 -0700

    Removed old comments

--- src/index.js
+++ src/index.js
@@ -14,15 +14,6 @@ root.render(
   </React.StrictMode>
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App text="this is app" />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

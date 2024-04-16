f97f89045155ee0076dd643767fc60c6463f6e8d
Austin Comstock <austinjcomstock@gmail.com>
2024-04-16 12:39:29 -0600

    Updated Experience section with list and new text to match resume

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -12,16 +12,22 @@ export const Experience = () => {
             <div className="subheading mb-3">Articulate LLC</div>
             <p>
               Responsible for building, testing and maintaining responsive
-              websites using HTML, CSS, JavaScript and React.js. Focus of
-              communication and collaboration with stakeholders to ensure design
-              and other requirements are met.
-              <br></br>
-              <br></br>
-              Implementing interactive features, such as forms and carousels,
-              using JavaScript and Bootstrap. Improve application performance by
-              optimizing images, implementing lazy loading while building with a
-              focus on search engine optimization.
+              websites.{" "}
             </p>
+            <ul>
+              <li>
+                Utilizing HTML, CSS, JavaScript and React.js to design front-end
+                and implement visually appealing, responsive and interactive web
+                pages. Building with a focus on search engine optimization.
+              </li>
+              <br></br>
+              <li>
+                Prioritize communication and collaboration with stakeholders to
+                ensure user-friendly interfaces that meet design requirements
+                and exceed expectations. Managing project versions effectively
+                using version control systems.
+              </li>
+            </ul>
           </div>
           <div className="flex-shrink-0">
             <span className="text-primary">March 2022 - Current</span>
@@ -32,23 +38,25 @@ export const Experience = () => {
             <h3 className="mb-0">Operations Manager</h3>
             <div className="subheading mb-3">Enova International</div>
             <p>
-              Co-led startup of new Utah office. After the office and teams were
-              established, I led a cross-functional team of fifteen
-              representatives. The team goal was to onboard new customers by
-              assisting them with personal loans. I was successful here by using
-              my communication and analytic skills to review data, identify
-              trends and drive results based on the data.
-              <br></br>
-              <br></br>
-              Due to the nature of the cross-functional team, knowledge of
-              several different processes and procedures were required. This was
-              a challenge because the information for these processes and
-              procedures were spread across different locations. After
-              identifying this I spearheaded the creation of an internal Wiki
-              page to house the content. This significantly improved the ramp-up
-              time for leadership to onboard and train new employees on
-              processes due to the content being available in one location.
+              Recruited to co-lead and manage the startup of a new Utah office.
+              After the office and teams were established, led a
+              cross-functional team of fifteen representatives.
             </p>
+            <ul>
+              <li>
+                The team goal was to onboard new customers by assisting them
+                with personal loans. Successful here by using communication and
+                analytic skills to review data, identify trends and drive
+                results based on the data.
+              </li>
+              <br></br>
+              <li>
+                Identified need and spearheaded the creation of an internal Wiki
+                page to house training content. This decreased the ramp-up time
+                for leadership to onboard and train new employees on processes
+                by 50% due to the content being available in one location.
+              </li>
+            </ul>
           </div>
           <div className="flex-shrink-0">
             <span className="text-primary">February 2020 - March 2022</span>
@@ -59,17 +67,28 @@ export const Experience = () => {
             <h3 className="mb-0">Quality Assurance Manager</h3>
             <div className="subheading mb-3">Upwell Health</div>
             <p>
-              Hired on to restructure QA department. Successfully improved call
-              auditing percentage with 75% less staff, increasing from 6% of
-              total calls audited to 30% by leading the implementation of a call
-              auditing software to support the limited staff.
-              <br></br>
-              <br></br>
-              Played a major role in creating processes within the compliance
-              and human resource departments to identify disciplinary guidelines
-              for representatives failing to meet compliance standards along
-              with recognition processes to highlight outstanding performance.
+              Promoted internally to restructure Quality Assurance department by
+              implementing call auditing software.
             </p>
+            <ul>
+              <li>
+                Successfully increased the call auditing percentage with 75%
+                less staff, increasing from 6% of total calls audited to 40% by
+                leading the implementation of a call auditing software to
+                support the limited staff. This decreased compliance risk while
+                increasing customer satisfaction and improving the bandwidth of
+                the legal team.
+              </li>
+              <br></br>
+              <li>
+                Played a major role in creating processes within the Compliance
+                and Human Resource departments to identify disciplinary
+                guidelines for representatives failing to meet compliance
+                standards along with recognition processes to highlight
+                outstanding performance. Resulting in greater collaboration and
+                positive relationships between departments.
+              </li>
+            </ul>
           </div>
           <div className="flex-shrink-0">
             <span className="text-primary">October 2016 - April 2019</span>
@@ -81,16 +100,23 @@ export const Experience = () => {
             <div className="subheading mb-3">Alliance Health</div>
             <p>
               Led team whose focus was to review and resolve escalated
-              complaints from customers threating or taking legal action against
-              the company. Developed reporting to track the complaint pipeline
-              along with a secondary process to escalate high level complaints
-              to the executive group.
-              <br></br>
-              <br></br>
-              Drafted response letters addressed to BBB, AG, BOP and other
-              agencies for our in-house counsel to resolve complaints resulting
-              in higher business ratings and customer satisfaction.
+              complaints from customers threatening to, or taking legal action
+              against the company.
             </p>
+            <ul>
+              <li>
+                Developed reports to track the complaint pipeline along with a
+                secondary process to escalate high level complaints to the
+                executive group. This brought awareness to the leadership team
+                on business process that needed improvement.
+              </li>
+              <br></br>
+              <li>
+                Drafted response letters addressed to BBB, AG, BOP and other
+                agencies for the in-house counsel to resolve complaints
+                resulting in higher business ratings and customer satisfaction.
+              </li>
+            </ul>
           </div>
           <div className="flex-shrink-0">
             <span className="text-primary">October 2015 - October 2016</span>
